399346ff388dbcfd5ea5d7944dd7b3648c0141da
Felipe Ros Pegini <56928106+Felipe-16@users.noreply.github.com>
2022-10-01 20:24:24 -0300

    Inserindo padrões de projeto

---
+++ flutter_application_1/lib/shared/Themes/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final brandingBlue = Color(0xFF022893);
+  static final lightBlue = Color(0xFF025AC8);
+  static final background = Color(0xFFFFFFFF);
+  static final generalLetter = Color(0xFF151A27);
+  static final lightLetter = Color(0xFF494950);
+  static final shape = Color(0xFFE1E1E5);
+  static final white = Colors.white;
+
+  static final BoxShadow clickShadow = BoxShadow(
+    color: Colors.black.withOpacity(0.3),
+    blurRadius: 4,
+    offset: Offset(0.0, 4),
+  );
+}
