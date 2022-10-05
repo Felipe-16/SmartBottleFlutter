6a47403036dcd1ea3089746b64b1690a37050270
Felipe Ros Pegini <felipe16699@gmail.com>
2022-10-04 23:47:05 -0300

    Bindando repository mock

--- flutter_application_1/lib/app/modules/home/home_module.dart
+++ flutter_application_1/lib/app/modules/home/home_module.dart
@@ -1,11 +1,13 @@
 import 'package:flutter_application_1/app/modules/home/home_controller.dart';
 import 'package:flutter_application_1/app/modules/home/home_page.dart';
+import 'package:flutter_application_1/app/modules/home/repository/home_repository.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind.lazySingleton((i) => HomeController()),
+    Bind.lazySingleton((i) => HomeController(repository: i())),
+    Bind.lazySingleton<HomeRepository>((i) => HomeRepository()),
   ];
 
   @override
