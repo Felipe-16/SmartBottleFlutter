0f8d05ac966666b75c78a88a5e8d53816a81faf0
Felipe Ros Pegini <felipe16699@gmail.com>
2022-10-04 23:47:24 -0300

    Implementando lista de mesas no controller

--- flutter_application_1/lib/app/modules/home/home_controller.dart
+++ flutter_application_1/lib/app/modules/home/home_controller.dart
@@ -1,7 +1,23 @@
+import 'package:flutter_application_1/app/modules/home/model/home_model.dart';
+import 'package:flutter_application_1/app/modules/home/repository/home_repository_interface.dart';
 import 'package:mobx/mobx.dart';
 
 part 'home_controller.g.dart';
 
-class HomeController = _HomeController with _$HomeController;
+class HomeController = _HomeControllerBase with _$HomeController;
 
-abstract class _HomeController with Store {}
+abstract class _HomeControllerBase with Store {
+  final IHomeRepository repository;
+
+  _HomeControllerBase({required this.repository}) {
+    getMesas();
+  }
+
+  @observable
+  List<HomeModel> listaMesas = List.empty();
+
+  @action
+  Future<void> getMesas() async {
+    listaMesas = await repository.getMesas();
+  }
+}
--- flutter_application_1/lib/app/modules/home/home_controller.g.dart
+++ flutter_application_1/lib/app/modules/home/home_controller.g.dart
@@ -8,11 +8,35 @@ part of 'home_controller.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$HomeController on _HomeController, Store {
+mixin _$HomeController on _HomeControllerBase, Store {
+  late final _$listaMesasAtom =
+      Atom(name: '_HomeControllerBase.listaMesas', context: context);
+
+  @override
+  List<HomeModel> get listaMesas {
+    _$listaMesasAtom.reportRead();
+    return super.listaMesas;
+  }
+
+  @override
+  set listaMesas(List<HomeModel> value) {
+    _$listaMesasAtom.reportWrite(value, super.listaMesas, () {
+      super.listaMesas = value;
+    });
+  }
+
+  late final _$getMesasAsyncAction =
+      AsyncAction('_HomeControllerBase.getMesas', context: context);
+
+  @override
+  Future<void> getMesas() {
+    return _$getMesasAsyncAction.run(() => super.getMesas());
+  }
+
   @override
   String toString() {
     return '''
-
+listaMesas: ${listaMesas}
     ''';
   }
 }
